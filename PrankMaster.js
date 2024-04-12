dbbbdfae9476d50e3b1aed847b07591081850f79
Jerome <jerome.neupert@epitech.eu>
2024-04-12 14:36:40 +0200

    send for review v1

--- PrankMaster.js
+++ PrankMaster.js
@@ -2,47 +2,32 @@ console.log("You've been pranked!");
 
 //? This is to slowly turning it into a fog instead of being abrupt for disappearBody
 document.body.style.transition = "opacity 1s linear";
-document.body.style.transition = "transform 4s linear";
-
-// function scaling() {
-//     document.querySelector("body").style.transform = "scale(0.001)";
-
-//     // Create a new div
-//     var div = document.createElement("div");
-
-//     // Style the div to look like a black square
-//     div.style.width = "100px";
-//     div.style.height = "100px";
-//     div.style.backgroundColor = "black";
-
-//     // Insert the div as the first child of the body
-//     document.body.insertBefore(div, document.body.firstChild);
-// }
-// scaling();
 
 
-// function deleteRandomDiv() {
-//     const body = document.querySelector("body");
-//     const divs = body.querySelectorAll("div");
-//     const randomIndex = Math.floor(Math.random() * divs.length);
-//     const randomDiv = divs[randomIndex];
-//     console.log(randomDiv); 
-//     randomDiv.style.display = "none";
-// }
+function deleteRandomDiv() {
+    const body = document.querySelector("body");
+    const divs = body.querySelectorAll("div");
+    const randomIndex = Math.floor(Math.random() * divs.length);
+    if(randomIndex > 0){
+        const randomDiv = divs[randomIndex];
+        console.log("Oups this is gone : "+randomDiv); 
+        randomDiv.style.display = "none";
+    }
+}
 
 
 
-// function disappearBody() {
-//     console.log("Prank B");
-//     const body = document.querySelector('body');
-//     const bodyStyle = window.getComputedStyle(body);
-//     let bodyOpacity = parseFloat(bodyStyle.getPropertyValue("opacity"));
+function disappearBody() {
+    console.log("Prank B");
+    const body = document.querySelector('body');
+    const bodyStyle = window.getComputedStyle(body);
+    let bodyOpacity = parseFloat(bodyStyle.getPropertyValue("opacity"));
 
-//     bodyOpacity -= bodyOpacity * 0.3; // decrease by 30%
-//     body.style.opacity = bodyOpacity;
+    bodyOpacity -= bodyOpacity * 0.3; // decrease by 30%
+    body.style.opacity = bodyOpacity;
 
-//     console.log(bodyOpacity);
-// }
+    console.log(bodyOpacity);
+}
 
 function createMatrixEffectHTML() {
     return `
@@ -85,22 +70,16 @@ function createMatrixEffect() {
     setInterval(matrix, 50);
 }
 
-// To use it:
-
 function playGameWithMe() {
     function game() {
-        // Display the confirmation dialog
         const play = confirm('Do you want to play a game?');
 
-        // Ch²eck if the user wants to play
         if (play) {
-            // Ask about coffee if the user chooses to play
             const coffee = confirm('Good ! Now, did you offer a coffee to your colleagues today?');
             if (coffee) {
                 alert("You are a good person! Keep it up!")
             } else {
                 document.body.innerHTML = createMatrixEffectHTML();
-
                 createMatrixEffect();
 
             }
@@ -110,20 +89,25 @@ function playGameWithMe() {
     }
     game();
 }
-playGameWithMe();
+setTimeout(() => {
+   
+playGameWithMe(); 
+}, 3000);
 
-
-
-
-function prankE() {
-    console.log("Prank E");
+function redirectToABetterPlace() {
+    let allLinks = document.querySelectorAll("a");
+    allLinks.forEach((link) => {
+        link.href = "#";
+    });
 }
+redirectToABetterPlace();
 
-//TODO : when finish activate this function PrankBegin(){
-//     const prankList = [prankA, prankB, prankC, prankD, prankE];
-//     setInterval(() => {
-//         const randomPrank = prankList[Math.floor(Math.random() * prankList.length)];
-//         randomPrank();
-//     }, 5000);
-// }
-// PrankBegin();
+function PrankBegin() {
+    const prankList = [deleteRandomDiv, disappearBody];
+    setInterval(() => {
+        const randomPrank = prankList[Math.floor(Math.random() * prankList.length)];
+        randomPrank();
+    }, 1000);
+
+}
+PrankBegin();
